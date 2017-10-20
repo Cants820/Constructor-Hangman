655beb497a0164ab3080d5d37e5bcf87ac0ba37b
Cants820 <Cantosjoseph@gmail.com>
2017-10-19 19:26:57 -0700

    hangman game 80% finished

--- hangman.js
+++ hangman.js
@@ -4,60 +4,101 @@
 // once the user guesses a letter for a conditional would to fill the word if an underscorce or decrement guesses left counter
 //constructor ??
 
-// game object
 
-var hangmanGame = {
-    wordBank: ["vanilla", "chocolate","napoleon","mocha","macha"], //ice cream flavors
-    wins: 0,
-    guessesLeft: 9,
-}
+//load the game using prompts
+//select a random word from the word object
+//once word has been selected create a new instance of the word in inside a new array with the letters
+
+
+// creates hangman constructor
+function HangmanGame() {
+  this.wordBank = ["vanilla", "chocolate","napoleon","mocha","macha","mango"];
+  this.wins = 0;
+  this.guessesLeft = 9;
+  this.wordSelector = this.wordBank[Math.floor(Math.random() * this.wordBank.length)];
+  }
+
+var newGame = new HangmanGame();
+
 
 //Word Constructor
-
+//Used to create an object representing the current word the user is attempting to guess. This should contain word specific logic and data.
 function Word (word) {
-  
+  this.word = word;
+  this.wordGuessArray = []; //place the word in a array
+  this.letterMaker = function() {
+    for(var i = 0; i < this.word.length;i++) {
+    this.wordGuessArray[i] = this.word.charAt(i); 
+   // console.log(this.wordGuessArray);
+    }
+  }
+}
+for (var i = 0; i < wordGuessArray.length; i++) {
+  if(userInput === wordGuessArray.indexof(userInput)) { //lookout includes
+  //array that contains random word in underscores
+  //compare wordGuessArray with the underscoresArray
+  wordGuessArray.splice(i,1,"");
+  underscoresArray.splice(i,1,"");
+  //userInput then replaces correct letter in random word
+  }
 }
 
+
+var word = new Word(newGame.wordSelector); //takes the word from Hangman game and sticks it inside the Word Constructor
+//word.letterMaker(); 
+
+//Letter Constructor
+// Used for each letter in the current word. Each letter object should either display an underlying character, or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. This should contain letter specific logic and data.
+function Letter(word){
+  this.letterArray = [];
+  this.word = word;
+  this.wordDashes = function(){ //results _ _ _ _ _ _ based on word selected from hangman game object
+    for (var i = 0; i < this.word.length; i++){
+        
+      // this.letterArray.push(i);
+
+      // console.log(letterArray);
+      //this.letterArray[i] = "_";
+            
+    }
+      
+  };
+}
 
 
 //randomizers
-//var wordSelector = wordArray[Math.floor(Math.random() * wordArray.length)];
+
 // console.log(wordSelector);
 
 
 
 
 
-var answerArray =[] //stores the answers array
-
-function wordDashes () {//turns selected word into dashes
-
-  for(var i = 0; i < wordSelector.length; i++) {
-    answerArray[i] = "_";
-   
-  }
- console.log(answerArray);
-}
 
 
-
-
-
-// //load game 
+// // //load game  using prompt npm
 // var prompt = require("prompt");
-  
+// var colors =require("colors/safe");
+
+// prompt.message=colors.green("?"); // removes prompt and replace with "?"
+
+
 // prompt.start();
  
 //   // 
 //   // Get two properties from the user: username and email 
 //   // 
-//   prompt.get(['Guess a letter'], function (err, result) {
-//     // 
-//     // Log the results. 
-//     // 
-//     console.log('Command-line input received:');
-//     console.log('  username: ' + result.username);
-//     console.log('  email: ' + result.email);
+//   prompt.get({
+//     properties: {
+//       name: {
+//         description: colors.white("Guess a Letter ?") //input message
+//       }
+//     }
+//   }, function (err, result) { //results
+
+//     //enter code... for hangman
+
+//     console.log(colors.cyan("You guessed wrong"));
 //   });
 
 
---
+++ hangmanV1.js
@@ -0,0 +1,110 @@
+//starts the game 
+//show the random word but display as _
+  // -create an array of random words
+  // -use Math.random to choice a word for the array
+
+//user would input a letter
+// type a letter and compare it to the -letters an array
+// -correct letter would display dash
+// -incorrect letter decrement guesses left
+
+//user guess the display word above or fails
+
+
+
+
+
+//load the game
+
+var wordBank = ["vanilla", "chocolate","napoleon", "mocha","macha","mango"];
+var wins = 0;
+var guessLeft = 9;
+var wordSelector = wordBank[Math.floor(Math.random() * wordBank.length)];
+var wordToGuessArray = [];//an array that contains letter of the randomWord chosen
+var dashesArray = [];//turns the random word letters into an array
+ // wordsToGuessArray = ["c","a","t"];
+ // dashesArray = ["_","_","_"];
+ // userInput = "a";
+
+function Letter(randomWord) {
+  this.randomWord = randomWord;
+  this.letterMaker = function() {
+    for(var i =0; i < this.randomWord.length;i++){
+       wordToGuessArray[i] = this.randomWord.charAt(i);
+    }
+     return wordToGuessArray;
+  }
+  this.dashesMaker = function() {
+    for(var i = 0; i < this.randomWord.length;i++){
+      dashesArray[i] = "_";
+    }
+     return dashesArray;
+  }
+}
+
+// var newGame1 = new Letter(wordSelector);
+// newGame1.letterMaker();
+    
+function Word(wordToGuessArray,dashesArray, userInput){
+    
+
+    if(wordToGuessArray.includes(userInput)){
+      console.log("correct letter");
+      //console.log(wordsToGuessArray.includes(userInput));
+      //console.log("Correct Guess " + correctGuess);
+      //var correctGuess = wordsToGuessArray.splice(index,1,"");
+      var index = wordToGuessArray.indexOf(userInput); //returns an index of the guessed number
+      var removedElement = wordToGuessArray[index]; // returns element of the 
+      
+      wordToGuessArray[index] = "";
+      // c a t 
+      
+      dashesArray[index] = removedElement;
+      // console.log(dashesArray[index] = removedElement);
+      // - a -
+      console.log(dashesArray);
+      
+
+    } else {
+      console.log("Incorrect Letter");
+      guessLeft--;
+      console.log(guessLeft);
+    }
+
+      // console.log(correctGuess);//
+      //  dashesArray.splice(i,1, correctGuess);
+      // console.log(correctGuess);
+}
+
+
+
+// console.log(newGame.letterMaker());
+//load game  using prompt npm
+// var randomWord = new Letter(wordSelector);
+  
+ var inquirer = require("inquirer");
+function Hangman() {
+
+    inquirer.prompt([
+      {
+        type:"input",
+        name: "wordguess",
+        message: "Please Input a Letter ?",
+      }
+    ]).then(function(answer) {
+
+    //console.log(randomWord.dashesMaker());
+    //console.log(randomWord.letterMaker());
+    // console.log(randomWord.letterMaker());
+
+    Word(newGame.letterMaker(),newGame.dashesMaker(), answer.wordguess);
+    
+
+    // Word(wordsToGuessArray,dashesArray,"a");
+    Hangman();
+    });
+
+}
+
+var newGame = new Letter(wordSelector);
+Hangman();
